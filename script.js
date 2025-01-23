c3725a74373000f7f26a16c032037dc4c276f8fa
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-23 12:52:17 +0100

    ADD normal exercise

---
+++ script.js
@@ -0,0 +1,64 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+// Ciao ragazzi, esercizio di oggi: Our Team
+// repo: js-our-team
+// Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+// (trovate l’array del team all’interno della cartella in allegato)
+// Bonus
+// Rendere l’esercizio responsive, mandando a capo le card
+// Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! :sorridere:)
+// Buon Lavoro
+
+let cardContainer = document.getElementById('card-container')
+
+for( i=0; i < teamMembers.length; i++){
+  let element = teamMembers[i]
+
+  cardContainer.innerHTML=
+  `         <div class="flex">
+                <figure>
+                    <img height="90px" src="./${element.img}" alt="">
+                </figure>
+                <div class="flex flex-column">
+                    <h5>${element.name}</h5>
+                    <span>${element.email}</span>
+                    <span>${element.role}</span>
+                </div>
+            </div>`
+}
